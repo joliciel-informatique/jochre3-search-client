8369d5f77ac22fdcc2fb478a80fdb7c0948ffe28
Martin Uildriks <muildriks@yiddishbookcenter.org>
2024-07-29 09:52:16 -0400

    Changes to header

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -1,19 +1,17 @@
 <script setup lang="ts">
-import { ref } from 'vue'
-import PreferencesModal from '@/_components/Modals/Preferences/PreferencesModal.vue'
-import { useKeycloakStore } from '@/stores/KeycloakStore'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import {
-  // import icons here
-  faGear,
-  faRightToBracket,
-  faRightFromBracket
-} from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+  import { ref } from 'vue'
+  import PreferencesModal from '@/_components/Modals/Preferences/PreferencesModal.vue'
+  import { useKeycloakStore } from '@/stores/KeycloakStore'
+  import { library } from '@fortawesome/fontawesome-svg-core'
+  import {
+    // import icons here
+    faGear,
+    faRightToBracket,
+    faRightFromBracket
+  } from '@fortawesome/free-solid-svg-icons'
+  import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-library.add(faGear)
-library.add(faRightToBracket)
-library.add(faRightFromBracket)
+library.add(faGear, faRightToBracket, faRightFromBracket)
 
 const keycloak = useKeycloakStore().keycloak
 
@@ -41,17 +39,21 @@ function loginOrLogout() {
 
 <template>
   <div
-    class="container is-fluid has-text-white custom-background has-text-weight-semibold m-0 p-0 header-footer-content"
+    class="container hery-body is-fluid has-text-white custom-background has-text-weight-semibold m-0 p-0 header-footer-content"
   >
     <div class="grid">
       <div class="cell p-2">
-        <img style="width: 100px; border: none" :src="$t('header.logo')" />
+        <div class="is-pulled-left">
+          <img style="width: 100px; border: none" :src="$t('header.logo')" />
+        </div>
       </div>
       <div class="cell is-col-span-2 p-2">
-        <div v-html="$t('header.title')"></div>
+        <div class="title is-3 has-text-white">{{ $t('header.title') }}</div>
+        <div class="beta-flyout">{{ $t('header.beta') }}</div>
+        <div class="link"><a class="link-white" href="{{ $t('header.link.url') }}" target="_blank">{{ $t('header.link.text') }}</a></div>
       </div>
       <div class="cell p-2">
-        <div class="container">
+        <div class="is-pulled-right">
           <button
             @click="showPreferences"
             class="button is-small"
