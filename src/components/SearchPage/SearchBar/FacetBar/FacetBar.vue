84a3efd5a71e1f7474a6413305c6a0ee3857c89d
Martin Uildriks <muildriks@yiddishbookcenter.org>
2024-09-13 15:28:23 -0400

    Facet tooltip

--- src/components/SearchPage/SearchBar/FacetBar/FacetBar.vue
+++ src/components/SearchPage/SearchBar/FacetBar/FacetBar.vue
@@ -10,26 +10,41 @@ Methods: None
 Description: presents the facet bar
 -->
 <template>
-  <div
-    id="facetBar"
-    v-if="!isLoading && facets.length"
-    class="container is-flex is-flex-direction-row is-justify-content-center is-flex-wrap-wrap"
-  >
-    <span v-for="facet of facets" v-bind:key="sha1(facet)">
-      <FilterTag
-        :label="facet.label"
-        :count="facet.count"
-        :active="facet.active"
-        :showCount="true"
-        @func="emit('newSearch', facet.label)"
-      />
-    </span>
+  <div id="facetBar" v-if="!isLoading && facets.length">
+    <hr />
+    <div class="columns has-text-centered is-vcentered">
+      <div class="column is-one-fifth">
+        <span
+          ><p>
+            Facets
+            <span v-tooltip:bottom="$t('search.what-are-facets')">
+              <FontAwesomeIcon icon="question-circle" />
+            </span>
+          </p>
+        </span>
+      </div>
+      <div
+        class="column container is-flex is-flex-direction-row is-justify-content-start is-flex-wrap-wrap"
+      >
+        <span v-for="facet of facets" v-bind:key="sha1(facet)">
+          <FilterTag
+            :label="facet.label"
+            :count="facet.count"
+            :active="facet.active"
+            :showCount="true"
+            @func="emit('newSearch', facet.label)"
+          />
+        </span>
+      </div>
+    </div>
+    <hr />
   </div>
 </template>
 <script setup lang="ts">
 import { type Ref } from 'vue'
 import FilterTag from '@/_components/FilterTag/FilterTag.vue'
 import { sha1 } from 'object-hash'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 const isLoading = defineModel('isLoading')
 const facets: Ref = defineModel('facets')
