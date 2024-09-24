ed898b360351bbc7cb8aaacca0215721069e7f18
Martin Uildriks <muildriks@yiddishbookcenter.org>
2024-09-24 10:40:35 -0400

    Dynamic imports on HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -35,16 +35,25 @@
 
 <script setup lang="ts">
 import { ref } from 'vue'
-import HeaderPage from '@/components/HeaderPage/HeaderPage.vue'
-import SearchPage from '@/components/SearchPage/SearchPage.vue'
-import FooterPage from '@/components/FooterPage/FooterPage.vue'
+const HeaderPage = () => import('@/components/HeaderPage/HeaderPage.vue')
+const SearchPage = () => import('@/components/SearchPage/SearchPage.vue')
+const FooterPage = () => import('@/components/FooterPage/FooterPage.vue')
+const LargeImage = () => import('@/_components/Modals/LargeImage/LargeImage.vue')
+const FixMetaData = () => import('@/_components/Modals/FixMetaData/FixMetaData.vue')
+const FixWord = () => import('@/_components/Modals/FixWord/FixWord.vue')
+const PreferencesSetup = () => import('@/_components/Modals/Preferences/PreferencesSetup.vue')
+const Preferences = () => import('@/_components/Modals/Preferences/PreferencesModal.vue')
+const Notification = () => import('@/_components/Notifications/AppNotification/AppNotification.vue')
+// import HeaderPage from '@/components/HeaderPage/HeaderPage.vue'
+// import SearchPage from '@/components/SearchPage/SearchPage.vue'
+// import FooterPage from '@/components/FooterPage/FooterPage.vue'
+// import LargeImage from '@/_components/Modals/LargeImage/LargeImage.vue'
+// import FixMetaData from '@/_components/Modals/FixMetaData/FixMetaData.vue'
+// import FixWord from '@/_components/Modals/FixWord/FixWord.vue'
+// import Notification from '@/_components/Notifications/AppNotification/AppNotification.vue'
+// import PreferencesSetup from '@/_components/Modals/Preferences/PreferencesSetup.vue'
+// import Preferences from '@/_components/Modals/Preferences/PreferencesModal.vue'
 import type { SearchResult } from '@/assets/interfacesExternals'
-import LargeImage from '@/_components/Modals/LargeImage/LargeImage.vue'
-import FixMetaData from '@/_components/Modals/FixMetaData/FixMetaData.vue'
-import FixWord from '@/_components/Modals/FixWord/FixWord.vue'
-import Notification from '@/_components/Notifications/AppNotification/AppNotification.vue'
-import PreferencesSetup from '@/_components/Modals/Preferences/PreferencesSetup.vue'
-import Preferences from '@/_components/Modals/Preferences/PreferencesModal.vue'
 import { usePreferencesStore } from '@/stores/PreferencesStore'
 
 const preferences = usePreferencesStore()
@@ -59,7 +68,7 @@ const imageModal = ref({})
 const metadataModal = ref({ field: 'author' })
 const wordModal = ref({})
 const notification = ref({})
-const searchPageRef = ref<InstanceType<typeof SearchPage>>()
+const searchPageRef = ref()
 
 // Methods exposed by SearchPage
 const resetSearchResults = () => searchPageRef.value?.resetSearchResults()
