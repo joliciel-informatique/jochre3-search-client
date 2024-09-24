4358f4c98cfdd4c9581224ae9c97be24da0ac376
Martin Uildriks <muildriks@yiddishbookcenter.org>
2024-09-24 11:12:03 -0400

    Dynamic import to HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -34,17 +34,30 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
-import HeaderPage from '@/components/HeaderPage/HeaderPage.vue'
-import SearchPage from '@/components/SearchPage/SearchPage.vue'
-import FooterPage from '@/components/FooterPage/FooterPage.vue'
+import { ref, defineAsyncComponent } from 'vue'
+const HeaderPage = defineAsyncComponent(() => import('@/components/HeaderPage/HeaderPage.vue'))
+const SearchPage = defineAsyncComponent(() => import('@/components/SearchPage/SearchPage.vue'))
+const FooterPage = defineAsyncComponent(() => import('@/components/FooterPage/FooterPage.vue'))
+const LargeImage = defineAsyncComponent(
+  () => import('@/_components/Modals/LargeImage/LargeImage.vue')
+)
+const FixMetaData = defineAsyncComponent(
+  () => import('@/_components/Modals/FixMetaData/FixMetaData.vue')
+)
+const FixWord = defineAsyncComponent(() => import('@/_components/Modals/FixWord/FixWord.vue'))
+const Notification = defineAsyncComponent(
+  () => import('@/_components/Notifications/AppNotification/AppNotification.vue')
+)
+const PreferencesSetup = defineAsyncComponent(
+  () => import('@/_components/Modals/Preferences/PreferencesSetup.vue')
+)
+const Preferences = defineAsyncComponent(
+  () => import('@/_components/Modals/Preferences/PreferencesModal.vue')
+)
+
 import type { SearchResult } from '@/assets/interfacesExternals'
-import LargeImage from '@/_components/Modals/LargeImage/LargeImage.vue'
-import FixMetaData from '@/_components/Modals/FixMetaData/FixMetaData.vue'
-import FixWord from '@/_components/Modals/FixWord/FixWord.vue'
-import Notification from '@/_components/Notifications/AppNotification/AppNotification.vue'
-import PreferencesSetup from '@/_components/Modals/Preferences/PreferencesSetup.vue'
-import Preferences from '@/_components/Modals/Preferences/PreferencesModal.vue'
+// import PreferencesSetup from '@/_components/Modals/Preferences/PreferencesSetup.vue'
+// import Preferences from '@/_components/Modals/Preferences/PreferencesModal.vue'
 import { usePreferencesStore } from '@/stores/PreferencesStore'
 
 const preferences = usePreferencesStore()
