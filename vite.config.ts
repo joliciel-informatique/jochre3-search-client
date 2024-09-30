892b223c8807d8c7e2d76588708e4a91faf8dbc2
Assaf Urieli <assaf.urieli@gmail.com>
2024-09-30 11:23:51 +0200

    Add CSP header to nginx configuration
    Add nonce to vite scripts.
    
    Replace vite nonce with nginx-generated nonce in nginx configuration via sub_filter.
    
    Issue #84

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,37 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(({ mode }) => {
+  const nonce = mode === 'development' ? 'nonce' : 'NGINX_CSP_NONCE'
+
+  // regex'es to add CSP
+  const regexScript = /<script(.*?)/gi
+  const replacementScript = `<script nonce="${nonce}"$1`
+
+  const regexStyle = /<style(.*?)/gi
+  const replacementStyle = `<style nonce="${nonce}"$1`
+
+  const regexLink = /<link(.*?)/gi
+  const replacementLink = `<link nonce="${nonce}"$1`
+
+  return {
+    plugins: [
+      vue(),
+      {
+        name: 'html-inject-nonce-into-script-tag',
+        enforce: 'post',
+        transformIndexHtml(html) {
+          return html
+            .replace(regexScript, replacementScript)
+            .replace(regexStyle, replacementStyle)
+            .replace(regexLink, replacementLink)
+        }
+      }
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
     }
   }
 })
