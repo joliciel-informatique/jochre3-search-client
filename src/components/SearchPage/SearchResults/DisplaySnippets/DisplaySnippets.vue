cc499a30b4300825cdff3ea74ad52dcddc92487c
Martin Uildriks <muildriks@yiddishbookcenter.org>
2024-11-11 16:40:14 -0500

    ToC and snippets synchronized

--- src/components/SearchPage/SearchResults/DisplaySnippets/DisplaySnippets.vue
+++ src/components/SearchPage/SearchResults/DisplaySnippets/DisplaySnippets.vue
@@ -16,41 +16,41 @@ Description: controls text snippets from the OCR text
       <!-- On desktop -->
       <div class="snippets-on-desktop is-hidden-touch">
         <ul>
-          <li v-for="(result, bookIndex) of searchResults" :key="sha1(result)" :bookIndex>
+          <li v-for="(result, bookIndex) of searchResults" :key="sha1(result)">
             <div v-if="!displayPerBook">
               <ul v-show="selectedEntry?.docRef === result.docRef">
-                <li v-for="(snippet, index) in result.snippets" :key="sha1(snippet)">
-                  <SingleSnippet
-                    v-model:image-modal="imageModal"
-                    v-model:word-modal="wordModal"
-                    v-model:notification="notification"
-                    v-model:selected-entry="selectedEntry"
-                    :index="index"
-                    :bookIndex="bookIndex"
-                    :snippet="snippet"
-                    :docRef="result.docRef"
-                    :url="result.url"
-                  />
-                </li>
+                <SingleSnippet
+                  v-for="(snippet, index) in result.snippets"
+                  :key="sha1(snippet)"
+                  :docRef="result.docRef"
+                  :bookIndex="bookIndex"
+                  :snippetIndex="index"
+                  :snippet="snippet"
+                  :url="result.url"
+                  v-model:image-modal="imageModal"
+                  v-model:word-modal="wordModal"
+                  v-model:notification="notification"
+                  v-model:selected-entry="selectedEntry"
+                />
               </ul>
             </div>
             <div v-else>
               <ul>
-                <li v-for="(snippet, index) in result.snippets" :key="sha1(snippet)" :bookIndex>
-                  <SingleSnippet
-                    v-model:image-modal="imageModal"
-                    v-model:word-modal="wordModal"
-                    v-model:notification="notification"
-                    v-model:selected-entry="selectedEntry"
-                    :index="index"
-                    :bookIndex="bookIndex"
-                    :snippet="snippet"
-                    :docRef="result.docRef"
-                    :url="result.url"
-                  />
-                </li>
+                <hr :bookindex="bookIndex" :id="`desktop-hr-${bookIndex}`" />
+                <SingleSnippet
+                  v-for="(snippet, index) in result.snippets"
+                  :key="sha1(snippet)"
+                  v-model:image-modal="imageModal"
+                  v-model:word-modal="wordModal"
+                  v-model:notification="notification"
+                  v-model:selected-entry="selectedEntry"
+                  :snippet="snippet"
+                  :docRef="result.docRef"
+                  :url="result.url"
+                  :bookIndex="bookIndex"
+                  :snippetIndex="index"
+                />
               </ul>
-              <hr />
             </div>
           </li>
         </ul>
@@ -59,41 +59,41 @@ Description: controls text snippets from the OCR text
       <!-- On mobile -->
       <div class="snippets-on-mobile is-hidden-desktop">
         <ul>
-          <li v-for="(result, bookIndex) of searchResults" :key="sha1(result)" :bookIndex>
+          <li v-for="(result, bookIndex) of searchResults" :key="sha1(result)">
             <div v-if="!displayPerBook">
               <ul v-show="selectedEntry?.docRef === result.docRef">
-                <li v-for="(snippet, index) in result.snippets" :key="sha1(snippet)">
-                  <SingleSnippet
-                    v-model:image-modal="imageModal"
-                    v-model:word-modal="wordModal"
-                    v-model:notification="notification"
-                    v-model:selected-entry="selectedEntry"
-                    :index="index"
-                    :bookIndex="bookIndex"
-                    :snippet="snippet"
-                    :docRef="result.docRef"
-                    :url="result.url"
-                  />
-                </li>
+                <SingleSnippet
+                  v-for="(snippet, index) in result.snippets"
+                  :key="sha1(snippet)"
+                  v-model:image-modal="imageModal"
+                  v-model:word-modal="wordModal"
+                  v-model:notification="notification"
+                  v-model:selected-entry="selectedEntry"
+                  :snippet="snippet"
+                  :docRef="result.docRef"
+                  :url="result.url"
+                  :bookIndex="bookIndex"
+                  :snippetIndex="index"
+                />
               </ul>
             </div>
             <div v-else>
               <ul>
-                <li v-for="(snippet, index) in result.snippets" :key="sha1(snippet)" :bookIndex>
-                  <SingleSnippet
-                    v-model:image-modal="imageModal"
-                    v-model:word-modal="wordModal"
-                    v-model:notification="notification"
-                    v-model:selected-entry="selectedEntry"
-                    :index="index"
-                    :bookIndex="bookIndex"
-                    :snippet="snippet"
-                    :docRef="result.docRef"
-                    :url="result.url"
-                  />
-                </li>
+                <hr :bookindex="bookIndex" :id="`mobile-hr-${bookIndex}`" />
+                <SingleSnippet
+                  v-for="(snippet, index) in result.snippets"
+                  :key="sha1(snippet)"
+                  v-model:image-modal="imageModal"
+                  v-model:word-modal="wordModal"
+                  v-model:notification="notification"
+                  v-model:selected-entry="selectedEntry"
+                  :snippet="snippet"
+                  :docRef="result.docRef"
+                  :url="result.url"
+                  :bookIndex="bookIndex"
+                  :snippetIndex="index"
+                />
               </ul>
-              <hr />
             </div>
           </li>
         </ul>
@@ -111,7 +111,7 @@ import { sha1 } from 'object-hash'
 import { usePreferencesStore } from '@/stores/PreferencesStore'
 import type { SearchResult } from '@/assets/interfacesExternals'
 import SingleSnippet from '../DisplaySnippets/SingleSnippet/SingleSnippet.vue'
-import { isInView } from '@/assets/functions'
+import { isInView, mobileCheck } from '@/assets/functions'
 import { onMounted } from 'vue'
 
 const preferences = usePreferencesStore()
@@ -125,13 +125,16 @@ const selectedEntry = defineModel<SearchResult>('selectedEntry')
 const searchResults = defineModel<SearchResult[]>('searchResults')
 const isLoading = defineModel('isLoading')
 
-// How many snippets for each volume are in view upon scroll only if shown in contiguous list
+// How many snippets for each volume are in view upon scroll only if shown in continuous list
 const scrolling = () => {
   if (displayPerBook.value) {
-    const snippetsInView = Array.from(document.querySelectorAll('li div .card.snippet'))
-      .map((snippet) => (isInView(snippet) ? snippet : null))
+    const snippets = !mobileCheck()
+      ? '.snippets-on-desktop li div .card.snippet'
+      : '.snippets-on-mobile li div .card.snippet'
+
+    const snippetsInView = Array.from(document.querySelectorAll(snippets))
+      .map((snippet) => (isInView(snippet) ? snippet.getAttribute('bookindex') : null))
       .filter((x) => x)
-      .map((snippet) => snippet?.getAttribute('bookindex'))
 
     const idx = snippetsInView
       .sort(
