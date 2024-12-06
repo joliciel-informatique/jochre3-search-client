a6d33cd7f0ccaa783476edb48c17df220f712a84
Assaf Urieli <assaf.urieli@gmail.com>
2024-12-06 11:36:55 +0100

    Move rtl-align back to top-most level

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,22 @@
 <template>
-  <PreferencesSetup />
-  <SearchPage
-    v-model:image-modal="imageModal"
-    v-model:word-modal="wordModal"
-    v-model:metadata-modal="metadataModal"
-    v-model:notification="notification"
-  />
-  <Preferences v-model:notification="notification" />
-  <LargeImage v-model:image-modal="imageModal" />
-  <FixWord v-model:word-modal="wordModal" v-model:notification="notification" />
-  <FixMetaData v-model:metadata-modal="metadataModal" v-model:notification="notification" />
-  <Notification v-model:notification="notification" />
+  <div
+    :class="{
+      'rtl-align': !preferences.displayLeftToRight
+    }"
+  >
+    <PreferencesSetup />
+    <SearchPage
+      v-model:image-modal="imageModal"
+      v-model:word-modal="wordModal"
+      v-model:metadata-modal="metadataModal"
+      v-model:notification="notification"
+    />
+    <Preferences v-model:notification="notification" />
+    <LargeImage v-model:image-modal="imageModal" />
+    <FixWord v-model:word-modal="wordModal" v-model:notification="notification" />
+    <FixMetaData v-model:metadata-modal="metadataModal" v-model:notification="notification" />
+    <Notification v-model:notification="notification" />
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -22,6 +28,10 @@ import FixWord from '@/_components/Modals/FixWord/FixWord.vue'
 import Notification from '@/_components/Notifications/AppNotification/AppNotification.vue'
 import PreferencesSetup from '@/_components/Modals/Preferences/PreferencesSetup.vue'
 import Preferences from '@/_components/Modals/Preferences/PreferencesModal.vue'
+
+import { usePreferencesStore } from '@/stores/PreferencesStore'
+
+const preferences = usePreferencesStore()
 
 // Props for modal boxes
 const imageModal = ref({})
